b80f816e6a006242df051671c889c72b90efef9a
samarinha565 <samararosa129@gmail.com>
2025-05-20 11:29:12 -0300

    Exercício Cadastro

---
+++ cadastro.py
@@ -0,0 +1,9 @@
+print ("Cadastro")
+nome = input("Nome: ")
+email = input("Email: ")
+senha = input("Senha: ")
+
+print("Cadastrado")
+print("Nome:",nome)
+print("Email:",email)
+print("Senha:",senha)
