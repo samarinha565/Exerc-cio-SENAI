0e867c7e3404745284a8ede084995b6c571ef91e
samarinha565 <samararosa129@gmail.com>
2025-05-20 11:51:47 -0300

    Exercício de Temperatura

---
+++ temperatura.py
@@ -0,0 +1,9 @@
+celsius = float(input("Digite a temperatura em Celsius: "))
+if celsius >= 30:
+    print("Está quente!")
+elif celsius >=20:
+    print("Está agradável!")
+elif celsius >=10:
+    print("Está frio!")
+else:
+    print("Está muito frio")            
