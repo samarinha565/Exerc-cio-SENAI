78e46d41c19fc5110f79df0eb063c3fb260c7ae0
samarinha565 <samararosa129@gmail.com>
2025-05-21 11:45:58 -0300

    While 2

---
+++ while_2.py
@@ -0,0 +1,25 @@
+from colorama import init, Fore
+init(autoreset=True)
+
+continuar = 's'
+while continuar == 's':
+
+ print("|",30*'-',"|")
+ print('| SISTEMA DE PROVAS')
+ print('|',30*'-',"|")
+
+ nome = input('| Nome do aluno: ')
+ n1 = float(input('| Nota da primeira prova: '))
+ n2 = float(input('| Nota da segunda prova: '))
+ n3 = float(input('| Nota da terceira prova: '))
+ print("|",30*'-',"|")
+ media = round((n1+n2+n3) /3, 2)  # round(calculo, número de casas decimais) round(media, 2)
+ print('|Aluno:', nome,'\n|Média:',media)
+ if media >=5:
+    print('|',Fore.GREEN + 'Aluno aprovado')
+ else:
+    print('|',Fore.RED + 'Aluno reprovado')
+ print("|",30*'-',"|")
+
+ continuar = input("Continuar? (s/n)")
+print("Finalizado.")
