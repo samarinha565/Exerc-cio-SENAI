bf169fd8f54ff988b54a5d011ad8f7a788b5e752
samarinha565 <samararosa129@gmail.com>
2025-06-04 10:54:43 -0300

    Tabuada

---
+++ tabuada.py
@@ -0,0 +1,10 @@
+
+contador = 1
+numero = int(input('Digite um número:'))
+fim = 10
+
+while contador <= fim:
+    print(f'{contador} X {numero} = {contador*numero}' )
+    contador = contador+1
+
+
